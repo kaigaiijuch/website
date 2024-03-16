7bf4eda3ea3aea5d5290e98c09449b9223f944e4
kibitan <uzukifirst@gmail.com>
2024-03-16 17:02:43 +0100

    feat: 🎸 use dotenv instead of hardcoding

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    config.google_tag_manager_id = ENV["GOOGLE_TAG_MANAGER_ID"]
+end
