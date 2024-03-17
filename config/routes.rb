3f50a3132b5bef26d84875a1e930788366dad725
kibitan <uzukifirst@gmail.com>
2024-03-17 14:20:18 +0100

    feat: 🎸 add redirect of feedback

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  get '/feedback', to: redirect('/feedbacks/new')
+  get '/feedback.html', to: redirect('/feedbacks/new')
   resources :feedbacks, only: %i[new]
 end
