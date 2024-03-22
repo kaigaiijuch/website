3c2c7bdd9750c55bc6a54b7785fa92e3d0c2f885
kibitan <uzukifirst@gmail.com>
2024-03-22 21:31:19 +0100

    fix: 🐛 wrong name of test class

--- test/lib/yaml_record_test.rb
+++ test/lib/yaml_record_test.rb
@@ -3,28 +3,30 @@
 require 'test_helper'
 require 'episodes_parser'
 
-class EpisodesParserTest < ActiveSupport::TestCase
-  class Sample < YamlRecord::Base
-    data_file 'test/fixtures/files/episodes.yml'
-  end
-  test 'inherited YamlRecord::Base class can set data_file' do
-    assert_equal 4, Sample.all.size
-    assert_raise(NotImplementedError) do
-      Sample.find('1-1')
+module YamlRecord
+  class BaseTest < ActiveSupport::TestCase
+    class Sample < YamlRecord::Base
+      data_file 'test/fixtures/files/episodes.yml'
     end
-  end
-
-  class Sample2 < YamlRecord::Base
-    data_file 'test/fixtures/files/episodes.yml'
-    attr_reader :title
-
-    def initialize(**attributes) # rubocop:disable Lint/MissingSuper
-      @title = attributes['title']
+    test 'inherited YamlRecord::Base class can set data_file' do
+      assert_equal 4, Sample.all.size
+      assert_raise(NotImplementedError) do
+        Sample.find('1-1')
+      end
     end
-  end
-  test 'inherited YamlRecord::Base instance can access attributes' do
-    assert_equal 4, Sample2.all.size
-    sample = Sample2.find('0')
-    assert_equal '#0 イントロダクション', sample.title
+
+    class Sample2 < YamlRecord::Base
+      data_file 'test/fixtures/files/episodes.yml'
+      attr_reader :title
+
+      def initialize(**attributes) # rubocop:disable Lint/MissingSuper
+        @title = attributes['title']
+      end
+    end
+    test 'inherited YamlRecord::Base instance can access attributes' do
+      assert_equal 4, Sample2.all.size
+      sample = Sample2.find('0')
+      assert_equal '#0 イントロダクション', sample.title
+    end
   end
 end
