c68c63f440d403a574351b460d5bbe7190b9aa28
kibitan <uzukifirst@gmail.com>
2024-03-26 17:04:58 +0100

    feat: 🎸 add embedurl generator

---
+++ test/helpers/episodes_helper_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module EpisodesHelperTest
+  class EmbedUrlHelperTest < ActionView::TestCase
+    setup do
+      @episode = Episode.find_by(key: '2-1')
+    end
+
+    test 'embed_url() should return embed url' do
+      assert_equal 'https://podcasters.spotify.com/pod/show/kaigaiijuch/embed/episodes/2-1-e2gujk0', embed_url(@episode)
+    end
+  end
+end
