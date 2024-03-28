f2a390aac46bce2ad11c285854a80addbb5c242e
kibitan <uzukifirst@gmail.com>
2024-03-28 13:59:16 +0100

    Revert "feat: 🎸 install active decorator"
    This reverts commit c06a263303bc1ec7e9844eeaa2551018a6fb92e0.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -6,14 +6,14 @@ class Episode < ApplicationYamlRecord
   def id = key
 
   def image_url
-    @image_url ||= URI.parse(@attributes[:image_url])
+    URI.parse(@attributes[:image_url])
   end
 
   def url
-    @url ||= URI.parse(@attributes[:url])
+    URI.parse(@attributes[:url])
   end
 
   def pub_date
-    @pub_date ||= Time.zone.parse(@attributes[:pub_date])
+    Time.zone.parse(@attributes[:pub_date])
   end
 end
