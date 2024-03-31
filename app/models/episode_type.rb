f07fbadd1a2f3a900e7182591877b281438bb047
kibitan <uzukifirst@gmail.com>
2024-03-31 22:19:33 +0200

    docs: ✏️ add podcast type infor

--- app/models/episode_type.rb
+++ app/models/episode_type.rb
@@ -14,5 +14,11 @@
 #  index_episode_types_on_name  (name) UNIQUE
 #
 class EpisodeType < ApplicationRecord
+  # this is defined in podcast: https://podcasters.apple.com/support/825-how-to-create-an-episode
+  #
+  # Full: The complete content of your show.
+  # Bonus: Extra content for your show (for example, behind-the-scenes information or interviews with the cast) or
+  #   cross-promotional content for another show. Bonus episodes can be available to anyone or only to paid subscribers.
+  # Trailer: A short, promotional piece of content that represents a preview of your current show.
   has_many :episodes, foreign_key: :type_id, inverse_of: :type # rubocop:disable Rails/HasManyOrHasOneDependent
 end
