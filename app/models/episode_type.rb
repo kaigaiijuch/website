4d4162ca2c14f9937a113b22aeec54765c94204e
kibitan <uzukifirst@gmail.com>
2024-04-01 16:26:57 +0200

    style: 💄 disable Rails/HasManyOrHasOneDependent

--- app/models/episode_type.rb
+++ app/models/episode_type.rb
@@ -20,5 +20,5 @@ class EpisodeType < ApplicationRecord
   # Bonus: Extra content for your show (for example, behind-the-scenes information or interviews with the cast) or
   #   cross-promotional content for another show. Bonus episodes can be available to anyone or only to paid subscribers.
   # Trailer: A short, promotional piece of content that represents a preview of your current show.
-  has_many :episodes, foreign_key: :type_id, inverse_of: :type # rubocop:disable Rails/HasManyOrHasOneDependent
+  has_many :episodes, foreign_key: :type_id, inverse_of: :type
 end
