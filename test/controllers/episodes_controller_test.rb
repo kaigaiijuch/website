5778c97fb75b5f010766684fc181c0da292973d3
kibitan <uzukifirst@gmail.com>
2024-04-02 23:21:43 +0200

    test: 💍 test all path of episodes

--- test/controllers/episodes_controller_test.rb
+++ test/controllers/episodes_controller_test.rb
@@ -3,8 +3,12 @@
 require 'test_helper'
 
 class EpisodesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @episodes = PublishedEpisode.all
+  end
+
   test 'should get show' do
-    PublishedEpisode.all.each do |episode|
+    @episodes.each do |episode|
       get episode_path(episode)
       assert_response :success
       assert_equal "/episodes/#{episode.number}", episode_path(episode)
