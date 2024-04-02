8376cc59fc08617b075c2051a059e36c2fa87e3b
kibitan <uzukifirst@gmail.com>
2024-04-02 16:25:51 +0200

    fix: 🐛 specify order of files and work without primary-key

--- bin/data/read_csv.rb
+++ bin/data/read_csv.rb
@@ -7,13 +7,15 @@ table_name = File.basename(file_path.split.last, '.csv')
 puts "Importing to #{table_name} from #{file_path}"
 
 csv = CSV.parse(File.read(file_path), headers: true)
-model = table_name.singularize.capitalize.constantize
+model = table_name.singularize.camelize.constantize
 
 puts "Importing #{csv.count} records to #{model}"
 
 csv.each do |row|
-  model.find_or_initialize_by(model.primary_key => row.fetch(model.primary_key)).tap do |record|
-    puts "Importing #{model} #{model.primary_key}: #{row.fetch(model.primary_key)}"
+  find_by_pair = model.primary_key.present? ? { model.primary_key => row.fetch(model.primary_key) } : row.to_h
+
+  model.find_or_initialize_by(find_by_pair).tap do |record|
+    puts "Importing #{model} #{find_by_pair}"
     record.attributes = row.to_h
     record.save!
   end
