175584ea283563fab7c9bcc36a766c67b83fbb89
kibitan <uzukifirst@gmail.com>
2024-04-03 11:19:19 +0200

    feat: 🎸 update list.rb for listing episode detail page

--- bin/pages/list.rb
+++ bin/pages/list.rb
@@ -4,4 +4,7 @@ Rails.application.routes.url_helpers.tap do |h|
   puts h.home_path
   puts h.feedback_path
   puts h.new_feedback_path
+  PublishedEpisode.find_each do |episode|
+    puts h.episode_path(episode)
+  end
 end
