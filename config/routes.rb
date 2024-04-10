cf0e91fb084a73e84324edaebc0eb3902544347f
kibitan <uzukifirst@gmail.com>
2024-04-10 16:05:55 +0200

    feat: 🎸 wip add not found page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'redirect/feedback'
   root 'home#index'
   get '/index', to: 'home#index', as: :home
+  get '/404', to: 'errors#not_found'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
