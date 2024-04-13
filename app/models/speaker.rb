23307f28ba18456f11ed62999454e4d0c684cba5
kibitan <uzukifirst@gmail.com>
2024-04-13 10:55:59 +0200

    docs: ✏️ add comment

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -12,6 +12,19 @@
 #  global_id  :string           not null
 #
 class Speaker < ApplicationRecord
+  # implemntation note:
+  #  there are other ways to implement for refer to Guest or Host
+  #  1. STI (Single Table Inheritance) https://guides.rubyonrails.org/association_basics.html#single-table-inheritance-sti
+  #    pros: rails standard way, guarantee to be only one host/guest
+  #    cons: no foreign key constraint (no guarantee to be hosts/guests exists), database cannot represent the relation
+  #          therefore sql become a bit complex.
+  #  2. relation table (e.g. guest_speakers, host_speakers)
+  #    pros: foreign key constrain (guarantee to be hosts/guests exists), database can represent the relation
+  #    cons: no guarantee to be only one host/guest
+  #  3. global_id (current implementation)
+  #    its' a bit similar to STI, but can represent the relation in one database column.
+  #  those are trade-offs, I choose as experimentabl to use GlobalID for now, i may change for the future.
+
   ALLOWED_WHO_TYPE = [Host, Guest].freeze
   validates_each :who do |record, attr, value|
     record.errors.add attr, 'it allows only Host or Guest' if ALLOWED_WHO_TYPE.map { |type| value.is_a?(type) }.none?
