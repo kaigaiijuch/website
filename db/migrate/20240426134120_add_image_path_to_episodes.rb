9aaf73cc8897786519f8f4d20ba4e26df05c2cfe
kibitan <uzukifirst@gmail.com>
2024-04-27 17:32:32 +0200

    refactor: 💡 change images to webp

--- db/migrate/20240426134120_add_image_path_to_episodes.rb
+++ db/migrate/20240426134120_add_image_path_to_episodes.rb
@@ -2,7 +2,7 @@
 
 class AddImagePathToEpisodes < ActiveRecord::Migration[7.1]
   def change
-    add_column :episodes, :image_path, :string, null: false, default: 'logos/logo_kaigaiiju-channel.png'
-    change_column_default :episodes, :image_path, from: 'logos/logo_kaigaiiju-channel.png', to: nil
+    add_column :episodes, :image_path, :string, null: false, default: 'logos/logo_kaigaiiju-channel.webp'
+    change_column_default :episodes, :image_path, from: 'logos/logo_kaigaiiju-channel.webp', to: nil
   end
 end
