7223953a4363af60e15fe168d17e2fd42b2af052
kibitan <uzukifirst@gmail.com>
2024-12-30 16:06:00 +0100

    fix: 🐛 update with edge case

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -22,13 +22,7 @@ module EpisodesHelper
   # if the link has ended with a parenthesis, it needs be escaped with a backslash
   #   e.g. https://en.wikipedia.org/wiki/Dave_Thomas_(programmer) -> [Dave Thomas](https://en.wikipedia.org/wiki/Dave_Thomas_(programmer\))
   def markdown_link(text)
-    case text
-    when MARKDOWN_LINK_REGEX_WITH_PARENTHESIS
-      text.gsub(MARKDOWN_LINK_REGEX_WITH_PARENTHESIS, link_to('\1', '\2)', target: '_blank', rel: 'noopener'))
-    when MARKDOWN_LINK_REGEX
-      text.gsub(MARKDOWN_LINK_REGEX, link_to('\1', '\2', target: '_blank', rel: 'noopener'))
-    else
-      text
-    end
+    text.gsub(MARKDOWN_LINK_REGEX_WITH_PARENTHESIS, link_to('\1', '\2)', target: '_blank', rel: 'noopener'))
+        .gsub(MARKDOWN_LINK_REGEX, link_to('\1', '\2', target: '_blank', rel: 'noopener'))
   end
 end
--- test/helpers/episodes_helper_test.rb
+++ test/helpers/episodes_helper_test.rb
@@ -77,8 +77,14 @@ module EpisodesHelperTest
       assert_equal expected_output, markdown_link(text)
     end
 
+    test 'markdown_link() should return linked html with escaped parenthes and another is not)' do
+      text = 'the [Andy Hunt](https://en.wikipedia.org/wiki/Andy_Hunt_(author\)) and [Another](https://example.org) !'
+      expected_output = 'the <a target="_blank" rel="noopener" href="https://en.wikipedia.org/wiki/Andy_Hunt_(author)">Andy Hunt</a> and <a target="_blank" rel="noopener" href="https://example.org">Another</a> !' # rubocop:disable Layout/LineLength
+      assert_equal expected_output, markdown_link(text)
+    end
+
     test 'markdown_link() should return linked html for multiple links' do
-      text = 'this is the [Link](http://example.com) for it and [Another](https://example.org)' # markdown link
+      text = 'this is the [Link](http://example.com) for it and [Another](https://example.org)'
       expected_output = 'this is the <a target="_blank" rel="noopener" href="http://example.com">Link</a> for it and <a target="_blank" rel="noopener" href="https://example.org">Another</a>' # rubocop:disable Layout/LineLength
       assert_equal expected_output, markdown_link(text)
     end
