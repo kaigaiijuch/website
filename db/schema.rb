6b51aa733a090ccec2ea3ecd1b0caebbfd7e12e3
kibitan <uzukifirst@gmail.com>
2025-01-10 09:29:24 +0100

    feat: 🎸 setup hosts data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_04_134904) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_10_074723) do
   create_table "answers", force: :cascade do |t|
     t.text "text", null: false
     t.date "answered_on", null: false
@@ -124,10 +124,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_04_134904) do
   end
 
   create_table "hosts", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "name", null: false
-    t.string "english_name", null: false
-    t.index ["nickname"], name: "index_hosts_on_nickname", unique: true
+    t.string "url"
+    t.string "title", null: false
+    t.string "description"
   end
 
   create_table "questions", primary_key: "number", id: :string, force: :cascade do |t|
@@ -169,28 +169,28 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_04_134904) do
 
   create_view "published_episodes", sql_definition: <<-SQL
       SELECT
-         *
-        FROM episodes
-        JOIN feeds_spotify_for_podcasters ON feeds_spotify_for_podcasters.episode_number = episodes.number
+       *
+      FROM episodes
+      JOIN feeds_spotify_for_podcasters ON feeds_spotify_for_podcasters.episode_number = episodes.number
   SQL
   create_view "questions_and_answers", sql_definition: <<-SQL
       SELECT *, answers.text AS answer_text, topics.name AS topic_name, questions.text AS question_text
-        FROM answers
-        JOIN questions ON answers.question_number = questions.number
-        JOIN topics ON questions.topic_code = topics.code
-        ORDER BY topics.display_order ASC, questions.display_order ASC
+      FROM answers
+      JOIN questions ON answers.question_number = questions.number
+      JOIN topics ON questions.topic_code = topics.code
+      ORDER BY topics.display_order ASC, questions.display_order ASC
   SQL
   create_view "episode_transcriptions", sql_definition: <<-SQL
       SELECT *
-        FROM episode_speaker_transcriptions
-        INNER JOIN episode_speakers ON episode_speaker_transcriptions.episode_speaker_id = episode_speakers.id
-        INNER JOIN speakers ON episode_speakers.speaker_id = speakers.id
-        ORDER BY episode_number, start_at
+      FROM episode_speaker_transcriptions
+      INNER JOIN episode_speakers ON episode_speaker_transcriptions.episode_speaker_id = episode_speakers.id
+      INNER JOIN speakers ON episode_speakers.speaker_id = speakers.id
+      ORDER BY episode_number, start_at
   SQL
   create_view "unpublished_episodes", sql_definition: <<-SQL
       SELECT *
-        FROM episodes
-        LEFT OUTER JOIN feeds_spotify_for_podcasters ON feeds_spotify_for_podcasters.episode_number = episodes.number
-        WHERE feeds_spotify_for_podcasters.episode_number IS NULL
+      FROM episodes
+      LEFT OUTER JOIN feeds_spotify_for_podcasters ON feeds_spotify_for_podcasters.episode_number = episodes.number
+      WHERE feeds_spotify_for_podcasters.episode_number IS NULL
   SQL
 end
--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -4,14 +4,11 @@
 #
 # Table name: hosts
 #
-#  id           :integer          not null, primary key
-#  english_name :string           not null
-#  name         :string           not null
-#  nickname     :string           not null
-#
-# Indexes
-#
-#  index_hosts_on_nickname  (nickname) UNIQUE
+#  id          :integer          not null, primary key
+#  description :string
+#  name        :string           not null
+#  title       :string           not null
+#  url         :string
 #
 require 'test_helper'
 
