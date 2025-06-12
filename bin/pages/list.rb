cb1153b286ae572e4c26ee11a584ccde382ff1d4
kibitan <uzukifirst@gmail.com>
2025-06-12 19:42:43 +0300

    feat: 🎸 only build essential page

--- bin/pages/list.rb
+++ bin/pages/list.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.url_helpers.tap do |h|
-  puts h.home_path
-  puts h.feedback_path
-  puts h.new_feedback_path
-  puts h.episodes_path
+  # puts h.home_path
+  # puts h.feedback_path
+  # puts h.new_feedback_path
+  # puts h.episodes_path
   puts h.episodes_path(format: :rss)
   PublishedEpisode.find_each do |episode|
     puts h.episode_path(episode)
