1ade5ccbba11de549add1958dcc18e959ab6c467
kibitan <uzukifirst@gmail.com>
2026-03-14 19:57:13 +0100

    test: 💍 failure

--- test/models/episode_photo_test.rb
+++ test/models/episode_photo_test.rb
@@ -19,10 +19,10 @@
 require 'test_helper'
 
 class EpisodePhotoTest < ActiveSupport::TestCase
-  test 'episode should have a relation with photo' do
-    assert episode_photos(:one), Episode.find('0').photo
-    assert episode_photos(:two), PublishedEpisode.find('1-1').photo
-    assert episode_photos(:three), UnpublishedEpisode.find('1-2').photo
+  test 'episode should have a relation with photos' do
+    assert episode_photos(:one), episodes(:one).photos.first
+    assert episode_photos(:two), PublishedEpisode.find('1-1').photos.first
+    assert episode_photos(:three), UnpublishedEpisode.find('1-2').photos.first
     assert episodes(:one), episode_photos(:one).episode
     assert episodes(:two), episode_photos(:two).episode
     assert episodes(:three), episode_photos(:three).episode
